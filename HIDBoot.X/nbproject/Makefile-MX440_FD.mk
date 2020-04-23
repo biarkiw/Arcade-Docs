d0cbb681ff970631fb24c1d46aeb2fe6023fd642
biarkiw <32373543+biarkiw@users.noreply.github.com>
2020-04-23 16:57:54 -0700

    Add files via upload

---
+++ HIDBoot.X/nbproject/Makefile-MX440_FD.mk
@@ -0,0 +1,193 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-MX440_FD.mk)" "nbproject/Makefile-local-MX440_FD.mk"
+include nbproject/Makefile-local-MX440_FD.mk
+endif
+endif
+
+# Environment
+MKDIR=gnumkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=MX440_FD
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/HIDBoot.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/HIDBoot.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+ifeq ($(COMPARE_BUILD), true)
+COMPARISON_BUILD=
+else
+COMPARISON_BUILD=
+endif
+
+ifdef SUB_IMAGE_ADDRESS
+
+else
+SUB_IMAGE_ADDRESS_COMMAND=
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=../Microchip/USB/usb_device.c "../Microchip/USB/HID Device Driver/usb_function_hid.c" main.c usb_descriptors.c
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/343710134/usb_device.o ${OBJECTDIR}/_ext/1295437596/usb_function_hid.o ${OBJECTDIR}/main.o ${OBJECTDIR}/usb_descriptors.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/343710134/usb_device.o.d ${OBJECTDIR}/_ext/1295437596/usb_function_hid.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/usb_descriptors.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/_ext/343710134/usb_device.o ${OBJECTDIR}/_ext/1295437596/usb_function_hid.o ${OBJECTDIR}/main.o ${OBJECTDIR}/usb_descriptors.o
+
+# Source Files
+SOURCEFILES=../Microchip/USB/usb_device.c ../Microchip/USB/HID Device Driver/usb_function_hid.c main.c usb_descriptors.c
+
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-MX440_FD.mk dist/${CND_CONF}/${IMAGE_TYPE}/HIDBoot.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=32MX440F256H
+MP_LINKER_FILE_OPTION=
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assembleWithPreprocess
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/_ext/343710134/usb_device.o: ../Microchip/USB/usb_device.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/343710134" 
+	@${RM} ${OBJECTDIR}/_ext/343710134/usb_device.o.d 
+	@${RM} ${OBJECTDIR}/_ext/343710134/usb_device.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/343710134/usb_device.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c ${MP_CC} $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -ffunction-sections -fdata-sections -mno-float -DFUBARDUINO -I".." -I"../Microchip/Include" -I"../Microchip/Include/USB" -I"./" -I"../HIDBoot.X/" -I"../.." -O1 -MMD -MF "${OBJECTDIR}/_ext/343710134/usb_device.o.d" -o ${OBJECTDIR}/_ext/343710134/usb_device.o ../Microchip/USB/usb_device.c   -fno-forward-propagate
+	
+${OBJECTDIR}/_ext/1295437596/usb_function_hid.o: ../Microchip/USB/HID\ Device\ Driver/usb_function_hid.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/1295437596" 
+	@${RM} ${OBJECTDIR}/_ext/1295437596/usb_function_hid.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1295437596/usb_function_hid.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/1295437596/usb_function_hid.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c ${MP_CC} $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -ffunction-sections -fdata-sections -mno-float -DFUBARDUINO -I".." -I"../Microchip/Include" -I"../Microchip/Include/USB" -I"./" -I"../HIDBoot.X/" -I"../.." -O1 -MMD -MF "${OBJECTDIR}/_ext/1295437596/usb_function_hid.o.d" -o ${OBJECTDIR}/_ext/1295437596/usb_function_hid.o "../Microchip/USB/HID Device Driver/usb_function_hid.c"   -fno-forward-propagate
+	
+${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c ${MP_CC} $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -ffunction-sections -fdata-sections -mno-float -DFUBARDUINO -I".." -I"../Microchip/Include" -I"../Microchip/Include/USB" -I"./" -I"../HIDBoot.X/" -I"../.." -O1 -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c   -fno-forward-propagate
+	
+${OBJECTDIR}/usb_descriptors.o: usb_descriptors.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/usb_descriptors.o.d 
+	@${RM} ${OBJECTDIR}/usb_descriptors.o 
+	@${FIXDEPS} "${OBJECTDIR}/usb_descriptors.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c ${MP_CC} $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -ffunction-sections -fdata-sections -mno-float -DFUBARDUINO -I".." -I"../Microchip/Include" -I"../Microchip/Include/USB" -I"./" -I"../HIDBoot.X/" -I"../.." -O1 -MMD -MF "${OBJECTDIR}/usb_descriptors.o.d" -o ${OBJECTDIR}/usb_descriptors.o usb_descriptors.c   -fno-forward-propagate
+	
+else
+${OBJECTDIR}/_ext/343710134/usb_device.o: ../Microchip/USB/usb_device.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/343710134" 
+	@${RM} ${OBJECTDIR}/_ext/343710134/usb_device.o.d 
+	@${RM} ${OBJECTDIR}/_ext/343710134/usb_device.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/343710134/usb_device.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c ${MP_CC} $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -ffunction-sections -fdata-sections -mno-float -DFUBARDUINO -I".." -I"../Microchip/Include" -I"../Microchip/Include/USB" -I"./" -I"../HIDBoot.X/" -I"../.." -O1 -MMD -MF "${OBJECTDIR}/_ext/343710134/usb_device.o.d" -o ${OBJECTDIR}/_ext/343710134/usb_device.o ../Microchip/USB/usb_device.c   -fno-forward-propagate
+	
+${OBJECTDIR}/_ext/1295437596/usb_function_hid.o: ../Microchip/USB/HID\ Device\ Driver/usb_function_hid.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/1295437596" 
+	@${RM} ${OBJECTDIR}/_ext/1295437596/usb_function_hid.o.d 
+	@${RM} ${OBJECTDIR}/_ext/1295437596/usb_function_hid.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/1295437596/usb_function_hid.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c ${MP_CC} $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -ffunction-sections -fdata-sections -mno-float -DFUBARDUINO -I".." -I"../Microchip/Include" -I"../Microchip/Include/USB" -I"./" -I"../HIDBoot.X/" -I"../.." -O1 -MMD -MF "${OBJECTDIR}/_ext/1295437596/usb_function_hid.o.d" -o ${OBJECTDIR}/_ext/1295437596/usb_function_hid.o "../Microchip/USB/HID Device Driver/usb_function_hid.c"   -fno-forward-propagate
+	
+${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.o.d 
+	@${RM} ${OBJECTDIR}/main.o 
+	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c ${MP_CC} $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -ffunction-sections -fdata-sections -mno-float -DFUBARDUINO -I".." -I"../Microchip/Include" -I"../Microchip/Include/USB" -I"./" -I"../HIDBoot.X/" -I"../.." -O1 -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c   -fno-forward-propagate
+	
+${OBJECTDIR}/usb_descriptors.o: usb_descriptors.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/usb_descriptors.o.d 
+	@${RM} ${OBJECTDIR}/usb_descriptors.o 
+	@${FIXDEPS} "${OBJECTDIR}/usb_descriptors.o.d" $(SILENT) -rsi ${MP_CC_DIR}../ -c ${MP_CC} $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -ffunction-sections -fdata-sections -mno-float -DFUBARDUINO -I".." -I"../Microchip/Include" -I"../Microchip/Include/USB" -I"./" -I"../HIDBoot.X/" -I"../.." -O1 -MMD -MF "${OBJECTDIR}/usb_descriptors.o.d" -o ${OBJECTDIR}/usb_descriptors.o usb_descriptors.c   -fno-forward-propagate
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+dist/${CND_CONF}/${IMAGE_TYPE}/HIDBoot.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -mdebugger -D__MPLAB_DEBUGGER_PK3=1 -mprocessor=$(MP_PROCESSOR_OPTION) -O1 -mno-float -o dist/${CND_CONF}/${IMAGE_TYPE}/HIDBoot.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}       -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__ICD2RAM=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_PK3=1,-L"..",-Map="${DISTDIR}/HIDBoot.X.${IMAGE_TYPE}.map" 
+else
+dist/${CND_CONF}/${IMAGE_TYPE}/HIDBoot.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -mprocessor=$(MP_PROCESSOR_OPTION) -O1 -mno-float -o dist/${CND_CONF}/${IMAGE_TYPE}/HIDBoot.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}       -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),-L"..",-Map="${DISTDIR}/HIDBoot.X.${IMAGE_TYPE}.map"
+	${MP_CC_DIR}\\pic32-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/HIDBoot.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r build/MX440_FD
+	${RM} -r dist/MX440_FD
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
---
+++ HIDBoot.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,27 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# MX460 configuration
+CND_ARTIFACT_DIR_MX460=dist/MX460/production
+CND_ARTIFACT_NAME_MX460=HIDBoot.X.production.hex
+CND_ARTIFACT_PATH_MX460=dist/MX460/production/HIDBoot.X.production.hex
+CND_PACKAGE_DIR_MX460=${CND_DISTDIR}/MX460/package
+CND_PACKAGE_NAME_MX460=hidboot.x.tar
+CND_PACKAGE_PATH_MX460=${CND_DISTDIR}/MX460/package/hidboot.x.tar
+# MX795 configuration
+CND_ARTIFACT_DIR_MX795=dist/MX795/production
+CND_ARTIFACT_NAME_MX795=HIDBoot.X.production.hex
+CND_ARTIFACT_PATH_MX795=dist/MX795/production/HIDBoot.X.production.hex
+CND_PACKAGE_DIR_MX795=${CND_DISTDIR}/MX795/package
+CND_PACKAGE_NAME_MX795=hidboot.x.tar
+CND_PACKAGE_PATH_MX795=${CND_DISTDIR}/MX795/package/hidboot.x.tar
+# MX440_FD configuration
+CND_ARTIFACT_DIR_MX440_FD=dist/MX440_FD/production
+CND_ARTIFACT_NAME_MX440_FD=HIDBoot.X.production.hex
+CND_ARTIFACT_PATH_MX440_FD=dist/MX440_FD/production/HIDBoot.X.production.hex
+CND_PACKAGE_DIR_MX440_FD=${CND_DISTDIR}/MX440_FD/package
+CND_PACKAGE_NAME_MX440_FD=hidboot.x.tar
+CND_PACKAGE_PATH_MX440_FD=${CND_DISTDIR}/MX440_FD/package/hidboot.x.tar
